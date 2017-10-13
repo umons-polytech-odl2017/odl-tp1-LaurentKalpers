c8c59c41c5e6116aba27b1399138744e3bde4636
LaurentKalpers <laurent.kalpers@student.umons.ac.be>
2017-10-13 16:29:27 +0200

    Personne implémentée

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,21 @@
 package exercise1;
 
 class Person {
+	private int age;
+	private String name;
+	public Person (int age, String name)
+	{
+		this.age=age;
+		this.name=name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+
 }
